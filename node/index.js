5e17cb334f6967598339b42510121a2424c8a5f1
大川遥平 <ohkawayohei@ookawaharukatairanoMac-mini.local>
2020-06-17 11:05:59 +0900

    FIX:nodejsのガイドファイルを記述

--- node/index.js
+++ node/index.js
@@ -1,6 +1,17 @@
 const http = require('http');
-const server = http.createServer(function(req, res) {
-   res.writeHead(200, {'ContentType': 'text/plain'});
-   res.end('HelloWorld');
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+   const endpoint = req.url;
+   if( endpoint==='/start' ){
+      fs.readFile('./index.html',(err, data)=>{
+         res.writeHead(200, {'Content-Type': 'text/html'});
+         res.write(data);
+         res.end();
+      });
+   }
+   if( endpoint==='/api' ){
+      // ここに処理を記述してください。  
+   }
 });
 server.listen(8080); 
